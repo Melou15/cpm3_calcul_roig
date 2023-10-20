44f778707cb4ed7784fcfe8e3eaf774affcb706e
Melissa Roig <melissa.roig@laposte.net>
2023-10-20 14:23:35 +0200

    tous fichiers réindentés

--- script.js
+++ script.js
@@ -4,54 +4,54 @@ var currentOperator = "";
 var memory = 0;
 
 document.querySelectorAll(".number").forEach(function (button) {
-    button.addEventListener("click", function () {
-        currentInput += button.textContent;
-        display.textContent = currentInput;
-    });
+	button.addEventListener("click", function () {
+		currentInput += button.textContent;
+		display.textContent = currentInput;
+	});
 });
 
 document.querySelectorAll(".operator").forEach(function (button) {
-    button.addEventListener("click", function () {
-        if (currentOperator !== "") {
-            calculate();
-        }
-        currentOperator = button.textContent;
-        memory = parseFloat(currentInput);
-        currentInput = "";
-    });
+	button.addEventListener("click", function () {
+		if (currentOperator !== "") {
+			calculate();
+		}
+		currentOperator = button.textContent;
+		memory = parseFloat(currentInput);
+		currentInput = "";
+	});
 });
 document.getElementById("calculate").addEventListener("click", calculate);
 
 document.getElementById("clear").addEventListener("click", function () {
-    currentInput = "";
-    currentOperator = "";
-    memory = 0;
-    display.textContent = "0";
+	currentInput = "";
+	currentOperator = "";
+	memory = 0;
+	display.textContent = "0";
 });
 
 function calculate() {
-    if (currentInput !== "") {
-        var currentValue = parseFloat(currentInput);
-        switch (currentOperator) {
-            case "+":
-                memory += currentValue;
-                break;
-            case "-":
-                memory -= currentValue;
-                break;
-            case "x":
-                memory *= currentValue;
-                break;
-            case "/":
-                if (currentValue !== 0) {
-                    memory /= currentValue;
-                } else {
-                    alert("Division par zéro !");
-                }
-                break;
-        }
-        display.textContent = memory;
-        currentInput = "";
-        currentOperator = "";
-    }
+	if (currentInput !== "") {
+		var currentValue = parseFloat(currentInput);
+		switch (currentOperator) {
+			case "+":
+			memory += currentValue;
+			break;
+			case "-":
+			memory -= currentValue;
+			break;
+			case "x":
+			memory *= currentValue;
+			break;
+			case "/":
+			if (currentValue !== 0) {
+				memory /= currentValue;
+			} else {
+				alert("Division par zéro !");
+			}
+			break;
+		}
+		display.textContent = memory;
+		currentInput = "";
+		currentOperator = "";
+	}
 }
